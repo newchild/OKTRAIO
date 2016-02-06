3bf45546a687ebf62dffbb7317aed924841b931e
newchild <fabianbrand98@gmx.de>
2016-02-06 19:41:56 +0100

    VelKoz early beta

---
+++ Champions/VelKoz.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using OKTRAIO;
+using OKTRAIO.Champions;
+using OKTRAIO.Menu_Settings;
+using OKTRAIO.Utility;
+using SharpDX;
+using Color = System.Drawing.Color;
+
+namespace OKTRAIO.Champions
+{
+	class VelKoz: AIOChampion
+	{
+		private Utility.Spell.Triangular _q;
+		private Spell.Skillshot _qFallBack, _w, _e, _r;
+		public override void Init()
+		{
+			throw new Exception("inDev");
+			MainMenu.ComboKeys(true, true, true, true);
+			MainMenu.HarassKeys(true, true, false, false);
+			_q = new Utility.Spell.Triangular(SpellSlot.Q, 250.0f, 250.0f, 1300.0f, 2100.0f, 1050.0f, 1050.0f, 45.0f);
+			_qFallBack = new Spell.Skillshot(SpellSlot.Q, 1050, SkillShotType.Linear, 250, 1300, 45);
+			_w = new Spell.Skillshot(SpellSlot.W, 1050, SkillShotType.Linear, 250, 1700, 80);
+			_e = new Spell.Skillshot(SpellSlot.E, 850, SkillShotType.Circular, 500, 1500, 120);
+			_r = new Spell.Skillshot(SpellSlot.R, 1550, SkillShotType.Linear);
+		}
+
+		public override void Combo()
+		{
+			Obj_AI_Base target = GetTarget();
+			if (target == null)
+				return;
+			_e.Cast(target);
+			_w.Cast(target);
+			AttemptCastQ(target);
+		}
+
+		//needs more selection logic
+		private void AttemptCastQ(Obj_AI_Base target)
+		{
+			if (_q.Cast(target.Position))
+				return;
+			_qFallBack.Cast(target);
+		}
+
+		private Obj_AI_Base GetTarget()
+		{
+			var targ = TargetSelector.GetTarget(_e.Range, DamageType.Magical);
+			if (targ != null)
+				return targ;
+			targ = TargetSelector.GetTarget(_w.Range, DamageType.Magical);
+			if (targ != null)
+				return targ;
+			targ = TargetSelector.GetTarget(_q.Range, DamageType.Magical);
+			if (targ != null)
+				return targ;
+			targ = TargetSelector.GetTarget(_r.Range, DamageType.Magical);
+			if (targ != null)
+				return targ;
+			return null;
+		}
+	}
+}
---
+++ Utility/TriangularSpell.cs
@@ -0,0 +1,82 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OKTRAIO.Utility.Spell
+{
+	class Triangular
+	{
+		private SpellSlot _slot;
+		private float _delay1, _delay2, _mspeed1, _mspeed2, _range1, _range2, _rangeMain, _spellWidth;
+		public Triangular(SpellSlot slot, float delay1, float delay2, float mspeed1, float mspeed2, float range1, float range2, float SpellWidth)
+		{
+			_slot = slot;
+			_delay1 = delay1;
+			_delay2 = delay2;
+			_mspeed1 = mspeed1;
+			_mspeed2 = mspeed2;
+			_range1 = range1;
+			_range2 = range2;
+			_spellWidth = SpellWidth;
+			_rangeMain = CalcLength(_range1, range2);
+		}
+
+		public float Range
+		{
+			get
+			{
+				return _rangeMain;
+			}
+		}
+
+		public bool Cast(Vector3 target)
+		{
+			if (Player.Instance.Distance(target) > _rangeMain)
+				return false;
+			float RecastDelay = CalcDelay(target);
+			Vector3 CastPos = CalcCastPos(target);
+			Player.CastSpell(_slot, CastPos);
+			Core.DelayAction(RecastSpell, (int)RecastDelay);
+			return true;
+		}
+
+		private void RecastSpell()
+		{
+			Player.CastSpell(SpellSlot.Q);
+		}
+
+		private float DegToRad(float deg)
+		{
+			return (float)(deg * (180.0 / Math.PI));
+		}
+
+		private Vector3 CalcCastPos(Vector3 target)
+		{
+			float len = CalcNeededLen(Player.Instance.Distance(target));
+			return (target - Player.Instance.Position).To2D().Rotated(DegToRad(45.0f)).To3D();
+		}
+
+		private float CalcDelay(Vector3 target)
+		{
+			float len = CalcNeededLen(Player.Instance.Distance(target));
+			return _delay1 + (len / _mspeed1);
+		}
+
+		private float CalcNeededLen(float length)
+		{
+			float q = length / 2.0f;
+			return (float)Math.Sqrt(q * length);
+		}
+
+
+		private float CalcLength(float _range1, float range2)
+		{
+			return (float)Math.Sqrt(Math.Pow(_range1, 2) + Math.Pow(_range2, 2));
+		}
+	}
+}
