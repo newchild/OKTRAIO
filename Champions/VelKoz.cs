e2225ff39ab60fa9ddb209aba31ef1797346b765
newchild <fabianbrand98@gmx.de>
2016-02-06 19:54:14 +0100

    Minor CompileFix

--- Champions/VelKoz.cs
+++ Champions/VelKoz.cs
@@ -17,14 +17,14 @@ namespace OKTRAIO.Champions
 {
 	class VelKoz: AIOChampion
 	{
-		private Utility.Spell.Triangular _q;
+		private Utility.Spells.Triangular _q;
 		private Spell.Skillshot _qFallBack, _w, _e, _r;
 		public override void Init()
 		{
 			throw new Exception("inDev");
 			MainMenu.ComboKeys(true, true, true, true);
 			MainMenu.HarassKeys(true, true, false, false);
-			_q = new Utility.Spell.Triangular(SpellSlot.Q, 250.0f, 250.0f, 1300.0f, 2100.0f, 1050.0f, 1050.0f, 45.0f);
+			_q = new Utility.Spells.Triangular(SpellSlot.Q, 250.0f, 250.0f, 1300.0f, 2100.0f, 1050.0f, 1050.0f, 45.0f);
 			_qFallBack = new Spell.Skillshot(SpellSlot.Q, 1050, SkillShotType.Linear, 250, 1300, 45);
 			_w = new Spell.Skillshot(SpellSlot.W, 1050, SkillShotType.Linear, 250, 1700, 80);
 			_e = new Spell.Skillshot(SpellSlot.E, 850, SkillShotType.Circular, 500, 1500, 120);
